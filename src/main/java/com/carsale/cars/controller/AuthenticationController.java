81c7ab6973228558db80806090e5d5fc9193ba2d
Harrisson Dutra <harrissondutra@gmail.com>
2024-08-26 22:32:37 -0300

    implementação do RestExceptionHandler

--- src/main/java/com/carsale/cars/controller/AuthenticationController.java
+++ src/main/java/com/carsale/cars/controller/AuthenticationController.java
@@ -5,7 +5,6 @@ import com.carsale.cars.model.User;
 import com.carsale.cars.model.records.UserLogin;
 import com.carsale.cars.service.TokenService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -18,18 +17,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/login")
 public class AuthenticationController {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
 
-    @Autowired
-    private TokenService tokenService;
+    private final AuthenticationManager authenticationManager;
+
+    private final TokenService tokenService;
+
+    public AuthenticationController(AuthenticationManager authenticationManager, TokenService tokenService) {
+        this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
+    }
 
     @PostMapping
-    public ResponseEntity login(@RequestBody @Valid UserLogin userLogin) throws Exception {
-        var authToken = new UsernamePasswordAuthenticationToken(userLogin.username(), userLogin.password());
-        var authentication = authenticationManager.authenticate(authToken);
-        var token = tokenService.generateToken((User) authentication.getPrincipal());
-        return ResponseEntity.ok(new TokenData(token));
-
+    public ResponseEntity<TokenData> login(@RequestBody @Valid UserLogin userLogin) {
+        try {
+            var authToken = new UsernamePasswordAuthenticationToken(userLogin.username(), userLogin.password());
+            var authentication = authenticationManager.authenticate(authToken);
+            var token = tokenService.generateToken((User) authentication.getPrincipal());
+            return ResponseEntity.ok(new TokenData(token));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new TokenData(e.getMessage()));
+        }
     }
 }
--- src/main/java/com/carsale/cars/infra/security/SpringConfiguration.java
+++ src/main/java/com/carsale/cars/infra/security/SpringConfiguration.java
@@ -2,8 +2,10 @@ package com.carsale.cars.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,6 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity(securedEnabled = true)
 @EnableWebSecurity
 public class SpringConfiguration {
 
@@ -29,6 +32,8 @@ public class SpringConfiguration {
                 .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorizeRequests -> {
                     authorizeRequests.requestMatchers("/login").permitAll();
+                    authorizeRequests.requestMatchers(HttpMethod.DELETE, "/users").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.DELETE,"/used-cars").hasRole("ADMIN");
                     authorizeRequests.anyRequest().authenticated();
                 })
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
